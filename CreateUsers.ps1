ebdcba25d695b9f5514ad7dd8ff9f5d6d052175e
cmcghee92 <chrs.mcghee@gmail.com>
2024-03-20 14:30:00 -0700

    Initial Commit

--- CreateUsers.ps1
+++ CreateUsers.ps1
@@ -6,10 +6,12 @@ $PWProfile = @{
         }
 
 New-MgUser `
-    -DisplayName "Chris McGhee" `
-    -GivenName "Chris" -Surname "McGhee" `
-    -MailNickname "cmcghee" `
+    -DisplayName "Ebone Burns" `
+    -GivenName "Ebone" -Surname "Burns" `
+    -MailNickname "eburns" `
     -UsageLocation "US" `
-    -UserPrincipalName "cmcghee@M365x46482142.onmicrosoft.com" `
+    -UserPrincipalName "eburns@M365x46482142.onmicrosoft.com" `
     -PasswordProfile $PWProfile -AccountEnabled `
     -Department "IT" -JobTitle "IT Administrator"
+
+    Get-mguser | Where-Object -FilterScript{$_.DisplayName -eq "Ebone Burns"}
