f90e85a11ed85bc68e29d051829a8d5749d3742f
cmcghee92 <chrs.mcghee@gmail.com>
2024-03-21 12:59:05 -0700

    testing

--- UpdateUserLocation.ps1
+++ UpdateUserLocation.ps1
@@ -1,9 +1,20 @@
 #Install-Module Microsoft.Graph -Scope CurrentUser
-Connect-MgGraph -Scopes "User.ReadWrite.All" 
+Connect-MgGraph -scopes "user.readwrite.all, group.readwrite.all"
 
-[string]$location = "United States"
+[string]$location = "US"
 [string]$phone = (Read-host -prompt "Please enter phone number")
 
 Get-MgUser | ForEach-Object { Update-MgUser -UserId $_.Id -UsageLocation "$Location" }
 
-Get-MgUser | ForEach-Object { Update-MgUser -UserId $_.Id -TelephoneNumber "$phone" }
+Get-MgUser | ForEach-Object { Update-MgUser -UserId $_.Id -MobilePhone "$phone" }
+
+
+Connect-MgGraph -scopes "user.read.all, user.readwrite.all, group.readwrite.all"
+[array]$CurrentPermissions = (Get-MgContext).Scopes
+[array]$RequiredPermissions = "User.Read.All"
+ForEach ($Permission in $RequiredPermissions) {
+    If ($Permission -notin $CurrentPermissions) {
+        Write-Host ("This script needs the {0} permission to run. Please have an administrator consent to the permission and try again" -f $Permission) 
+        Break
+    }
+}
